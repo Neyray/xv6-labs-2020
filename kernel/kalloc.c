da0aed7ccda04a54203f43bd5fbfba1529aba605
Neyray <zjr19374507113@outlook.com>
2026-04-17 01:09:32 -0700

    上传了lab8 memory allocate的部分

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -22,13 +22,18 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;   //空闲列表受到自旋锁（spin lock）的保护,列表和锁被封装在一个结构体中，以明确锁在结构体中保护的字段
-} kmem;
+} kmem[NCPU];    //把单个kmem改造成数组，每个CPU拥有独立的空闲链表+独立的锁
 
 //分配器开始时没有内存；这些对kfree的调用给了它一些管理空间
+//kinit只由一个CPU调用，所以freerange->free会把所有页面都挂在当前CPU的链表上，其他CPu需要时再去偷
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");    //kinit初始化空闲列表以保存从内核结束到PHYSTOP之间的每一页
+  char lockname[8];
+  for(int i=0;i<NCPU;++i){
+	  snprintf(lockname,sizeof(lockname),"kmem_%d",i);
+	  initlock(&kmem[i].lock, "kmem");    //kinit初始化空闲列表以保存从内核结束到PHYSTOP之间的每一页
+  }
   freerange(end, (void*)PHYSTOP);   //kinit调用freerange将内存添加到空闲列表中，在freerange中每页都会调用kfree
 }
 
@@ -62,10 +67,13 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();   //关中断（防止调用cpuid期间被调度到其他CPU）
+  int id=cpuid();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+  pop_off();    //开中断
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -77,11 +85,29 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int id=cpuid();
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  else{
+	  //当前CPU链表为空，遍历其他CPU偷一页
+	  for(int antid=0;antid<NCPU;++antid){
+		  if(antid==id)continue;   //这是遍历自己的情况
+		  acquire(&kmem[antid].lock);
+		  r=kmem[antid].freelist;
+		  if(r){
+			  //就从这里偷了
+			  kmem[antid].freelist=r->next;
+			  release(&kmem[antid].lock);
+			  break;
+		  }
+		  release(&kmem[antid].lock);
+	  }
+  }
+  release(&kmem[id].lock);
+  pop_off();    //打开中断
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
