7471cb37ec9079765bcb47456cb2d938e9ef94e2
Neyray <zjr19374507113@outlook.com>
2026-04-16 19:28:57 -0700

    对kernel/kalloc.c的内容添加了注释

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -13,21 +13,23 @@ void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
-
+//每个空闲页的列表元素是一个struct run
 struct run {
   struct run *next;
 };
 
+//将每个空闲页的run结构存储在空闲页本身
 struct {
   struct spinlock lock;
-  struct run *freelist;
+  struct run *freelist;   //空闲列表受到自旋锁（spin lock）的保护,列表和锁被封装在一个结构体中，以明确锁在结构体中保护的字段
 } kmem;
 
+//分配器开始时没有内存；这些对kfree的调用给了它一些管理空间
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  initlock(&kmem.lock, "kmem");    //kinit初始化空闲列表以保存从内核结束到PHYSTOP之间的每一页
+  freerange(end, (void*)PHYSTOP);   //kinit调用freerange将内存添加到空闲列表中，在freerange中每页都会调用kfree
 }
 
 void
@@ -43,6 +45,10 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
+//
+// 函数kfree (kernel/kalloc.c:47)首先将内存中的每一个字节设置为1。
+// 这将导致使用释放后的内存的代码（使用“悬空引用”）读取到垃圾信息而不是旧的有效内容，从而希望这样的代码更快崩溃
+// kfree将页面前置（头插法）到空闲列表中：它将pa转换为一个指向struct run的指针r，在r->next中记录空闲列表的旧开始，并将空闲列表设置为等于r
 void
 kfree(void *pa)
 {
@@ -65,6 +71,7 @@ kfree(void *pa)
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+// kalloc删除并返回空闲列表中的第一个元素。
 void *
 kalloc(void)
 {
